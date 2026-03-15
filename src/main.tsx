c9fae6a83a50ba85cb7c0fa44b6cca463e64d31a
yukki072417 <yukki072417@gmail.com>
2026-03-15 10:53:05 +0900

    HasuRouterを使用

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./features/App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
+  // </StrictMode>
 );
