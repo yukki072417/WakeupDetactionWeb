4d4ad319951fc496ddd1870a696a5125a2a3a4f1
yukki072417 <yukki072417@gmail.com>
2026-03-15 10:57:41 +0900

    いろいろ

--- src/features/App/App.tsx
+++ src/features/App/App.tsx
@@ -1,23 +1,5 @@
-import { useEffect } from "react";
-import { useAndroidBridge } from "../../common/hooks/useAndroidBridge";
-
 function App() {
-  const { send } = useAndroidBridge();
-
-  useEffect(() => {
-    // Vite dev server からデータを取得
-    fetch("http://10.0.2.2:3000/api/data")
-      .then((res) => res.json())
-      .then((data) => {
-        send(JSON.stringify(data)); // Android に送信
-      });
-  }, []);
-
-  return (
-    <div>
-      <button onClick={() => send("Hello from React")}>Send to Android</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
