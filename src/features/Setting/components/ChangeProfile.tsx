74799316fdd8dca4b0966fda204535612f39dcd3
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-16 01:46:29 +0900

    style: 設定ページ

--- src/features/Setting/components/ChangeProfile.tsx
+++ src/features/Setting/components/ChangeProfile.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./ChangeProfile.css";
+import IconProfile from "../../../assets/icon_profile.png";
+import IconPhoto from "../../../assets/icon_photo.png";
 
 const ChangeProfile = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <div className="profile_edit">
+      <div className="profile_edit" onClick={() => setShowModal(true)}>
         <h3>プロフィール編集</h3>
       </div>
+
       <div className="friend_count">
         <div>
           <h3>12</h3>
@@ -16,6 +22,29 @@ const ChangeProfile = () => {
           <p>寝坊回数</p>
         </div>
       </div>
+
+      {showModal && (
+        <div className="modal_overlay">
+          <picture>
+            <img src={IconProfile} alt="" />
+            <span></span>
+          </picture>
+          <div className="nameCard">
+            <h3>名前</h3>
+            <input type="text" placeholder="うえだあまれ" />
+          </div>
+          <div className="nameCard">
+            <h3>ユーザーID</h3>
+            <input type="text" placeholder="@ueda_amare" />
+          </div>
+          <div className="btn" onClick={() => setShowModal(false)}>
+            <h3>プロフィールを保存</h3>
+          </div>
+          <div className="btn nobtn" onClick={() => setShowModal(false)}>
+            <h3>キャンセル</h3>
+          </div>
+        </div>
+      )}
     </>
   );
 };
