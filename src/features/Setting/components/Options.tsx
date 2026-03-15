d59941e63823f3e0f1467d6f0f94c4060a4949a1
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-16 00:11:50 +0900

    style: toggleが動いたぜ

--- src/features/Setting/components/Options.tsx
+++ src/features/Setting/components/Options.tsx
@@ -1,10 +1,24 @@
 import "./Options.css";
+import IconAlarm from "../../../assets/icon_alarm.png";
 
 const Options = () => {
   return (
-    <div>
-      <p>適当太郎</p>
-    </div>
+    <>
+      <h2>設定</h2>
+      <div className="option_wrap">
+        <div className="option_box">
+          <img src={IconAlarm} alt="" />
+          <div>
+            <h3>起床通知</h3>
+            <p>起きる時間になったら通知を送信します</p>
+          </div>
+        </div>
+        <label className="toggle">
+          <input type="checkbox" />
+          <span className="slider"></span>
+        </label>
+      </div>
+    </>
   );
 };
 
